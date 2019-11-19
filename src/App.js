f24eb49b632cbf345826807a2e237ff99eccb7f7
CastagnaTM <51973817+CastagnaTM@users.noreply.github.com>
2019-11-19 11:10:41 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css';
+import Login from './Components/Login'
+import Signup from './Components/Signup'
+import HomeScreen from './Components/HomeScreen'
+import { Route, Switch } from 'react-router-dom';
+
+
+function App() {
+  return (
+    <div>
+      <Switch>
+        <Route path="/login" render={(routerProps) => <Login {...routerProps}/>} />
+        <Route path="/signup" render={(routerProps) => <Login {...routerProps}/>} />
+        <Route path="/home" render={(routerProps) => <Login {...routerProps}/>} />
+
+      </Switch>
+      {/* <Login/>
+      <Signup/> */}
+      <HomeScreen/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/HomeScreen.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+
+export default class HomeScreen extends Component{
+
+
+
+
+    render() {
+        return(
+            <div>
+                <h1>Hey</h1>
+            </div>
+        )
+    }
+
+}
