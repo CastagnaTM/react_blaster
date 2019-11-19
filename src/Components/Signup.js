1907cee73db37ade79b13790b4503402f2971911
castagnaTM <castagna.tm@gmail.com>
2019-11-19 11:24:26 -0500

    I hope this is fixed now

---
+++ src/Components/Signup.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+
+export default class Signup extends Component{
+
+
+
+
+    render() {
+        return(
+            <div>
+                <h1>Signup Screen</h1>
+            </div>
+        )
+    }
+
+}
