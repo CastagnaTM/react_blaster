4d31625cb8fbef3305da935627490731c7ea90ac
castagnaTM <castagna.tm@gmail.com>
2019-11-20 12:22:24 -0500

    set up basic tile display and CSS effects

--- src/Components/HomeScreen.js
+++ src/Components/HomeScreen.js
@@ -1,17 +1,54 @@
 import React, { Component } from 'react';
+import LevelContainer from '../Containers/LevelContainer'
 
 
 export default class HomeScreen extends Component{
 
+    state = {
+        play: false
+    }
+
+    // top of the screen = level select in game font
+    // everything shares a cool background
+    // the main of the page holds many components, and stylings for those components
+    levelSelect = () => {
+        return(
+            <div>
+                <div className='home-screen-header'>
+                    <h4>Select A Level</h4>
+                </div>
+
+                <div className='home-screen-body'>
+                    <button onClick={() => this.loadLevel()}>Start</button> {/* this will take info later to determine which level */}
+                </div>
+            </div>
+        )
+    }
+    loadLevel = () => {
+        this.setState({
+            play: true
+        })
+    }
+    play = () => {
+        return (
+                <LevelContainer/>
+        )
+    }
 
 
 
     render() {
-        return(
-            <div>
-                <h1>Hey There from HomeScreen</h1>
-            </div>
-        )
+        if (this.state.play === false){
+            return (
+                this.levelSelect()
+            )
+        } else {
+            return (
+                this.play()
+            )
+        }
     }
+        
+    
 
 }
