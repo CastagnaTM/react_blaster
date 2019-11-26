c8288fb462bf4a4f5cd508a40e432be7f53d7a16
castagnaTM <castagna.tm@gmail.com>
2019-11-26 10:36:18 -0500

    added bombs, explosions, health, and health meter

--- src/Components/LevelEnd.js
+++ src/Components/LevelEnd.js
@@ -3,13 +3,13 @@ import React, { Component } from 'react'
 export default class LevelEnd extends Component {
 
     handleReturn = () => {
-        this.props.levelComplete(this.props.success, this.props.levelPoints)
+        this.props.levelComplete(this.props.success, this.props.levelPoints, this.props.health)
     }
 
     render(){
         return(
-            <div className='tile-grid-container'>
-                <div className='tile-grid'>
+            <div className='level-end-container'>
+                <div className='level-end-grid'>
                     <div>
                         <p style={{color:'whitesmoke', textAlign: 'center'}}>
                             {this.props.success ? `Congrats! You Scored ${this.props.levelPoints} Points!` : 'Sorry, you lost this time around...'}
--- src/Components/Targets.js
+++ src/Components/Targets.js
@@ -3,6 +3,8 @@ import friendly1 from '../Assets/Friendly1.png'
 import friendlySad from '../Assets/FriendlySad.png'
 import Satellite from '../Assets/Satellite.png'
 import Explosion from '../Assets/Explosion.png'
+import Bomb from '../Assets/Bomb.png'
+import BombEx from '../Assets/BombEx.png'
 
 
 
@@ -34,6 +36,13 @@ export default class Targets extends Component{
             return(
                 <div className='tile-target' onClick={() => this.props.handleClick(this.props.name, this.props.target_type)} style={{backgroundColor: this.props.isClicked ? '#3F1923' : this.state.friendlyBackgroundColor}}>
                     <img className='tile-img' alt='friendly alien' src={this.props.isClicked ? friendlySad : friendly1}/>
+                </div>
+            )
+        }
+        if(this.props.target_type === 'bomb'){
+            return(
+                <div className='tile-target' onClick={() => this.props.handleClick(this.props.name, this.props.target_type)} style={{backgroundColor: this.props.isClicked ? '#3F1923' : this.state.debrisBackgroundColor}}>
+                    <img className='tile-img' alt='bomb' src={this.props.isClicked ? BombEx : Bomb}/>
                 </div>
             )
         }
