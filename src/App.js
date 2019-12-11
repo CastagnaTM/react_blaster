59327740df4f54f0f5e6054b069ff1eb0e43998e
castagnaTM <castagna.tm@gmail.com>
2019-12-11 15:38:02 -0500

    removed unnecessary browser routing

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 import TitleScreen from './Components/TitleScreen'
-import { Route, Switch } from 'react-router-dom';
 
 export default class App extends Component {
 
   render(){
     return (
     <div>
-      <Switch>
-        {/* <Route path="/" render={(routerProps) => <LandingPage setUser={this.setUser}{...routerProps}/>} /> */}
-        <Route exact path="/" render={(routerProps) => <TitleScreen {...routerProps}/>} />
-      </Switch>
+      <TitleScreen />
     </div>
   );
   }
